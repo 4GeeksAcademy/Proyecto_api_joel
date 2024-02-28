4f39ab10274c8934129c2575cbc7bdfeb5cd7b4b
Joel1695 <88430706+Joel1695@users.noreply.github.com>
2024-02-28 20:12:44 +0000

    terminado

--- src/app.py
+++ src/app.py
@@ -2,14 +2,41 @@ import os
 from sqlalchemy import create_engine
 import pandas as pd
 from dotenv import load_dotenv
-
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import numpy as np
+import matplotlib.pyplot as plt
 # load the .env file variables
 load_dotenv()
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+conection = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id = client_id,client_secret=client_secret))
+artist_id = '2AbQwU2cuEGfD465wCXlg2'
+response = conection.artist_top_tracks(artist_id)
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+data =[]
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+for track in response['tracks']:
+   
+    dic ={'name' : track['name'],
+          'popularity' : track['popularity'],
+          'duration_min': round((track['duration_ms'])/60000,2)}
+    data.append(dic)
+
+df = pd.DataFrame(data)
+
+df = df.sort_values(by='popularity',ascending=False)
+print(df.head(3))
+corelacion = df.corr()
+print(corelacion)
+
+
+
+plt.scatter(df['duration_min'], df['popularity'])
+plt.xlabel('Duración (min)')
+plt.ylabel('Popularidad')
+plt.title('Gráfico de dispersión: Duración vs Popularidad')
+
+plt.show()
